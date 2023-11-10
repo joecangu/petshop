aed62c7feb118e1480962ad122b62242c124939e
Jhonatan Canguçu <jhonatan.cangucu10@gmail.com>
2023-11-10 14:49:09 -0300

    Update firebaseConnection.js

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -5,13 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCXOCJJXhF-WyzU9DB7RsUQ05byfSF2zUc",
-    authDomain: "pets-8278e.firebaseapp.com",
-    projectId: "pets-8278e",
-    storageBucket: "pets-8278e.appspot.com",
-    messagingSenderId: "839955171671",
-    appId: "1:839955171671:web:d638bfb21b9daa00725612",
-    measurementId: "G-1G3ZW2V5BB"
+    
   };
   
   const firebaseApp = initializeApp(firebaseConfig);
